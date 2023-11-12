34e55e4c9a0527f2504fabbe78f5a021c4005cea
Hein <cantatasonatadev@gmail.com>
2023-11-13 01:18:30 +0630

    response format

---
+++ src/interceptors/response/response.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+export interface Response<T> {
+  statusCode: string;
+  message: string;
+  data: T;
+  meta: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: context.switchToHttp().getResponse().statusCode,
+        message: data.message,
+        data: data.data,
+        meta: data?.meta ?? {},
+      })),
+    );
+  }
+}
